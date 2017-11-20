bfbe8765da24d2aa44cdcddd2f80affd64dcc15a
Mudasir Younas <mudasiryounas@gmail.com>
2017-11-20 22:01:52 +0300

    Add files via upload

---
+++ MyS3Chat/DependencyResolution/StructureMapScopeModule.cs
@@ -0,0 +1,24 @@
+namespace MyS3Chat.DependencyResolution {
+    using System.Web;
+
+    using MyS3Chat.App_Start;
+
+    using StructureMap.Web.Pipeline;
+
+    public class StructureMapScopeModule : IHttpModule {
+        #region Public Methods and Operators
+
+        public void Dispose() {
+        }
+
+        public void Init(HttpApplication context) {
+            context.BeginRequest += (sender, e) => StructuremapMvc.StructureMapDependencyScope.CreateNestedContainer();
+            context.EndRequest += (sender, e) => {
+                HttpContextLifecycle.DisposeAndClearAll();
+                StructuremapMvc.StructureMapDependencyScope.DisposeNestedContainer();
+            };
+        }
+
+        #endregion
+    }
+}
