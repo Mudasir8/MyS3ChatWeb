143bd2d2e14b2a6d9b7f3c6a16c975f287dbcccc
Mudasir Younas <mudasiryounas@gmail.com>
2017-11-20 22:03:43 +0300

    Add files via upload

---
+++ MyS3Chat/Library/DAL/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using MyS3Chat.Library.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MyS3Chat.Library.DAL
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Friend> Friends { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Token> Tokens { get; set; }
+    }
+}
